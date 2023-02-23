3026827a9e68d9fe1c504627355e8d13eac71b96
Matthew Woods <matthew.woods0157@gmail.com>
2023-02-23 12:36:38 -0500

    Changes to the canvas context value

--- snake.js
+++ snake.js
@@ -80,3 +80,26 @@ function show() {
   update();
   draw();
 }
+
+function update() {
+
+}
+
+function draw() {
+  createRect(0, 0, canvas.width, canvas.height, "black");
+  createRect(0, 0, canvas.width, canvas.height)
+  for(var i = 0; i < snake.tail.length; i++) {
+    createRect(snake.tail[i].x + 2.5, snake.tail[i].y + 2.5, snake.size - 5, snake.size - 5, 'white');
+  }
+
+  canvasContext.font = '20px Arial';
+  canvasContext.fillStyle = '#00FF42';
+  canvasContext.fillText("Score: ", (snake.tail.length + 1),
+    canvas.width - 120, 18);
+  createRect(apple.x, apple.y, apple.size, apple.size, apple.color)
+}
+
+function createRect(x,y,width, height, color) {
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(x, y, width, height) 
+} 
