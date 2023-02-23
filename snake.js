2208256c1c99a58d96f7b6184e1233d8b122b52c
Matthew Woods <matthew.woods0157@gmail.com>
2023-02-23 12:24:19 -0500

    Added apple constructor

--- snake.js
+++ snake.js
@@ -1,5 +1,5 @@
 
-class Snake{
+class Snake {
   constructor(x, y, size) {
     this.x = x;
     this.y = y;
@@ -32,6 +32,30 @@ class Snake{
         y: this.tail[this.tail.length - 1].y - this.size
       }
     }
+
+    this.tail.shift();
+    this.tail.push(newRect);
+  }
+}
+
+class Apple {
+  constructor() {
+    var isTouching;
+    while(true) {
+      isTouching = false;
+      this.x = Math.floor(Math.random() * canvas.width / snake.size) * snake.size;
+      this.y = Math.floor(Math.random() * canvas.height / snake.size) * snake.size;
+      for(var i = 0; i < snake.tail.length; i++) {
+        if(this.x == snake.tail[i].x && this.y == snake.tail[i].y) {
+          isTouching = true;
+        }
+      }
+      if(!isTouching){
+        break;
+      }
+      this.color = "pink";
+      this.size = snake.size;
+    } 
   }
 }
 
